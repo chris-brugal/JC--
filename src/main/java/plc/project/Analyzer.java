f50f595457f1778046c1e39da6d7b2e3379fb559
Chris <chrisbrugal@yahoo.com>
2021-11-20 20:46:25 -0500

    finished half of analyzer

--- src/main/java/plc/project/Analyzer.java
+++ src/main/java/plc/project/Analyzer.java
@@ -2,6 +2,7 @@ package plc.project;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,8 +27,7 @@ public final class Analyzer implements Ast.Visitor<Void> {
 
     @Override
     public Void visit(Ast.Source ast) {
-
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -72,46 +72,192 @@ public final class Analyzer implements Ast.Visitor<Void> {
 
     @Override
     public Void visit(Ast.Statement.While ast) {
-        throw new UnsupportedOperationException();  // TODO
+        try {
+            Ast.Expression e = ast.getCondition();
+            visit(e);
+            requireAssignable(Environment.Type.BOOLEAN, e.getType());
+            try {
+                scope = new Scope(scope);
+                for (Ast.Statement s : ast.getStatements()){
+                    visit(s);
+                }
+            }
+            finally {
+                scope = scope.getParent();
+            }
+        }
+        catch (RuntimeException exception) {
+            throw new RuntimeException(exception);
+        }
+        return null;
     }
 
     @Override
     public Void visit(Ast.Statement.Return ast) {
-        throw new UnsupportedOperationException();  // TODO
+        visit(ast.getValue());
+        try {
+            requireAssignable(scope.lookupVariable("returnType").getType(), ast.getValue().getType());
+        }catch (RuntimeException exception) {
+            throw new RuntimeException(exception);
+        }
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.Literal ast) {
-        throw new UnsupportedOperationException();  // TODO
+        //to remove repeating
+        Object literal = ast.getLiteral();
+        if (literal == Environment.NIL){
+            ast.setType(Environment.Type.NIL);
+        } else if (literal instanceof Boolean){
+            ast.setType(Environment.Type.BOOLEAN);
+        }else if (literal instanceof Character){
+            ast.setType(Environment.Type.CHARACTER);
+        }else if (literal instanceof String) {
+            ast.setType(Environment.Type.STRING);
+        }else if(literal instanceof BigInteger) {
+            BigInteger literalInt = (BigInteger) literal;
+            if ((literalInt.intValueExact() > Integer.MAX_VALUE) || (literalInt.intValueExact() < Integer.MIN_VALUE)) {
+                throw new RuntimeException("Not in Range");
+            }
+            ast.setType(Environment.Type.INTEGER);
+        }else if (literal instanceof BigDecimal){
+            BigDecimal literalDec = (BigDecimal) literal;
+            if ((literalDec.doubleValue() == Double.NEGATIVE_INFINITY) || (literalDec.doubleValue() == Double.POSITIVE_INFINITY)) {
+                throw new RuntimeException("Not in Range");
+            }
+            ast.setType(Environment.Type.DECIMAL);
+        }
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.Group ast) {
-        throw new UnsupportedOperationException();  // TODO
+        visit(ast.getExpression());
+        try {
+            if (ast.getExpression().getClass() != Ast.Expression.Binary.class){
+                throw new RuntimeException("not a binary expression");
+            }
+        }catch (RuntimeException exception) {
+            throw new RuntimeException(exception);
+        }
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.Binary ast) {
-        throw new UnsupportedOperationException();  // TODO
+        Ast.Expression l = ast.getLeft();
+        Ast.Expression r =ast.getRight();
+        visit(l);
+        visit(r);
+        String operator = ast.getOperator();
+        //System.out.println("passed visits");
+
+        if(operator.equals("&&") || operator.equals("||")){
+            requireAssignable(Environment.Type.BOOLEAN, l.getType());
+            requireAssignable(Environment.Type.BOOLEAN, r.getType());
+            ast.setType(Environment.Type.BOOLEAN);
+        }else if (operator.equals("<") || operator.equals(">") || operator.equals("==") || operator.equals("!=")){
+            requireAssignable(Environment.Type.COMPARABLE, l.getType());
+            requireAssignable(Environment.Type.COMPARABLE, r.getType());
+            requireAssignable(l.getType(), r.getType());
+            ast.setType(Environment.Type.BOOLEAN);
+        }else if (operator.equals("+")){
+            if(l.getType() == Environment.Type.STRING || r.getType() == Environment.Type.STRING){
+                ast.setType(Environment.Type.STRING);
+            }else if (l.getType() == Environment.Type.INTEGER && r.getType() == Environment.Type.INTEGER){
+                ast.setType(Environment.Type.INTEGER);
+            }else if (l.getType() == Environment.Type.DECIMAL && r.getType() == Environment.Type.DECIMAL){
+                ast.setType(Environment.Type.DECIMAL);
+            }else{
+                throw new RuntimeException("Addition of wrong types");
+            }
+        }else if (operator.equals("-") || operator.equals("*") || operator.equals("/")){
+            if (l.getType() == Environment.Type.INTEGER && r.getType() == Environment.Type.INTEGER){
+                ast.setType(Environment.Type.INTEGER);
+            }else if (l.getType() == Environment.Type.DECIMAL && r.getType() == Environment.Type.DECIMAL){
+                ast.setType(Environment.Type.DECIMAL);
+            }else{
+                throw new RuntimeException("Sub/Mult/Div of wrong types");
+            }
+        }else if (operator.equals("^")){
+            if (l.getType() == Environment.Type.INTEGER && r.getType() == Environment.Type.INTEGER){
+                ast.setType(Environment.Type.INTEGER);
+            }else if (l.getType() == Environment.Type.DECIMAL && r.getType() == Environment.Type.INTEGER){
+                ast.setType(Environment.Type.DECIMAL);
+            }else{
+                throw new RuntimeException("Power of wrong types");
+            }
+        }else{
+            throw new RuntimeException("No binary");
+        }
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.Access ast) {
-        throw new UnsupportedOperationException();  // TODO
+        try {
+            //field
+            if (ast.getOffset().isPresent()) {
+                Ast.Expression.Access t = (Ast.Expression.Access) ast.getOffset().get();
+                t.setVariable(scope.lookupVariable(t.getName()));
+                try {
+                    String varName = t.getName();
+                    scope = scope.lookupVariable(varName).getType().getScope();
+                    Environment.Variable var = scope.lookupVariable(ast.getName());
+                    ast.setVariable(var);
+                }
+                finally {
+                    scope = scope.getParent();
+                }
+            }
+            //nonfield
+            else {
+                Environment.Variable var = scope.lookupVariable(ast.getName());
+                ast.setVariable(var);
+            }
+        }
+        catch (RuntimeException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.Function ast) {
-        throw new UnsupportedOperationException();  // TODO
+        Environment.Function f = scope.lookupFunction(ast.getName(), ast.getArguments().size());
+        List<Ast.Expression> arguments = ast.getArguments();
+        List<Environment.Type> types = f.getParameterTypes();
+        for (int i = 0; i < arguments.size(); i++) {
+            visit(arguments.get(i));
+            requireAssignable(types.get(i), arguments.get(i).getType());
+        }
+        ast.setFunction(f);
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.PlcList ast) {
-        throw new UnsupportedOperationException();  // TODO
+        try{
+            for(Ast.Expression e: ast.getValues()){
+                //not 100% sure
+                requireAssignable(e.getType(), ast.getType());
+            }
+        }catch (RuntimeException exception) {
+            throw new RuntimeException(exception);
+        }
+        return null;
     }
 
     public static void requireAssignable(Environment.Type target, Environment.Type type) {
-        throw new UnsupportedOperationException();  // TODO
+        try {
+            if (target != type && target != Environment.Type.ANY && target != Environment.Type.COMPARABLE)
+                throw new RuntimeException("Error: Types Do Not Match.");
+        }
+        catch (RuntimeException re) {
+            throw new RuntimeException(re);
+        }
     }
 
 }
